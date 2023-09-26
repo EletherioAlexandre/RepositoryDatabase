671d3690f048c69494db8badc6975671658a09bc
EletherioAlexandre <eletherioalexandre@gmail.com>
2023-09-26 17:43:19 -0300

    Implementação de Interface de Cliente e métodos na ClienteRepository

---
+++ Interfaces/IClienteRepository.cs
@@ -0,0 +1,23 @@
+﻿using CSharpProjeto01.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProjeto01.Interfaces
+{
+    /// <summary>
+    /// Interfaces para abstração de todos os métodos de repositório de dados relacionados aos clientes
+    /// </summary>
+    public interface IClienteRepository
+    {
+        #region Métodos abstratos
+        void InsertClient(Cliente cliente);
+        void UpdateClient(Cliente cliente);
+        void DeleteClient(Cliente cliente);
+        List<Cliente> GetAll();
+        Cliente? GetById(Guid id);
+        #endregion
+    }
+}
---
+++ Repositories/ClienteRepository.cs
@@ -0,0 +1,38 @@
+﻿using CSharpProjeto01.Entities;
+using CSharpProjeto01.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProjeto01.Repositories
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        public void DeleteClient(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Cliente> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente? GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertClient(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateClient(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

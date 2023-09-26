38e5f77b732f8e9d7ed156f15626e445849e6300
EletherioAlexandre <eletherioalexandre@gmail.com>
2023-09-26 17:13:26 -0300

    First Commit

---
+++ Entities/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProjeto01.Entities
+{
+    /// <summary>
+    /// Modelo de entidade
+    /// </summary>
+    public class Cliente
+    { 
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
